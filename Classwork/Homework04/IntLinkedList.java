b812efa3fbf4441f6a6c9a53f841926e933357ba
lindseyfry <lfry1@lion.lmu.edu>
2018-09-20 21:34:02 -0700

    Create IntLinkedList.java

---
+++ Classwork/Homework04/IntLinkedList.java
@@ -0,0 +1,110 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  IntLinkedList.java
+ *  @author       :  Lindsey Fry
+ *  Date written  :  2018-09-20
+ *
+ *  Notes         :  None right now.  I'll add some as they occur.
+ *  Warnings      :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ---------------
+ *            Rev      Date     Modified by:  Reason for change/modification
+ *           -----  ----------  ------------  -----------------------------------------------------------
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class IntLinkedList {
+
+   private Node head;
+   private int  size;
+
+  // the constructor
+   IntLinkedList() {
+      head = null;
+      size = 0;
+   }
+
+   public int getSize() {
+      return size;
+   }
+
+   public void insertAt( int index , int number ) {
+     if(index > size || index < 0){
+       throw new IllegalArgumentException("");
+     } else {
+      Iterator it = getIteratorAt( index );
+      Node newNode = new Node( number );
+      newNode.next = it.currentNode.next;
+      it.currentNode.next = newNode;
+    }
+    size++;
+   }
+
+   public void removeAt( int index ) {
+       if ( index > size -1 || index < 0 ) {
+           throw new IllegalArgumentException("Invalid index!");
+       }
+       Iterator it2 = getIteratorAt(index);
+       Node node = it2.currentNode.next;
+       Iterator myIt = getIteratorAt(index-1);
+       myIt.currentNode.next = node;
+       size--;
+       }
+
+   public void prepend( int dataToAdd ) {
+      Node currentHead = head;
+      head = new Node( dataToAdd );
+      head.next = currentHead;
+      size++;
+   }
+
+   private class Node {
+
+      int data;            // remember this is an IntLinkedList
+      Node next;           // here's the self-referential part
+
+      // constructor
+      Node( int d ) {
+         data = d;
+         next = null;
+      }
+   }
+
+   public Iterator getIteratorAt( int index ) {
+      if( (index > size) || (index < 0) ) {
+         throw new IllegalArgumentException();
+      }
+      Iterator it = new Iterator();
+      while( index > 0 ) {
+         it.next();
+         index--;
+      }
+      return it;
+   }
+
+
+   public class Iterator {
+      private Node currentNode;
+      //private Node newNode;
+
+      Iterator() {
+         currentNode = head;
+      }
+
+      public void next() {
+         if( currentNode == null ) {
+            return;
+         } else {
+            currentNode = currentNode.next;
+         }
+      }
+
+      public boolean hasNext() {
+         return ((currentNode != null) && (currentNode.next != null));
+      }
+
+      public int getCurrentInt() {
+         return currentNode.data;
+      }
+
+   }
+}
