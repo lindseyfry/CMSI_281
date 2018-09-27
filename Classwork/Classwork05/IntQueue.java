1b5d599c9340b2a6c9c48fa8940817c5b0413f3c
lindseyfry <lfry1@lion.lmu.edu>
2018-09-27 09:42:31 -0700

    Create IntQueue.java

---
+++ Classwork/Classwork05/IntQueue.java
@@ -0,0 +1,59 @@
+public class IntQueue {
+
+      IntLinkedList myQueue;
+
+      IntQueue() {
+         myQueue = new IntLinkedList();         // constructor
+      }
+
+      public void enQueue( int itemToEnQueue ) {
+         myQueue.prepend( itemToEnQueue );
+      }
+
+      public int peek() {
+         return myQueue.getIteratorAt( myQueue.getSize()-1 ).getCurrentInt();     // we use the iterator
+      }
+
+      public int deQueue() {
+         return myQueue.removeAt( myQueue.getSize()-1 );
+      }
+      public int getSize(){
+        return myQueue.getSize();
+      }
+      public String toString() {
+          String newString = "";
+          int index  = myQueue.getSize()-1;
+          while(index >= 0){
+            newString += myQueue.getIteratorAt(index).getCurrentInt() + " ";
+            index--;
+          }
+          return newString;
+      }
+
+      public static void main( String[] args ) {
+         IntQueue testQueue = new IntQueue();
+         testQueue.enQueue( 19 );
+         testQueue.enQueue( 23 );
+         testQueue.enQueue( 29 );
+         testQueue.enQueue( 31 );
+         testQueue.enQueue( 37 );
+         testQueue.enQueue( 41 );
+         testQueue.enQueue( 43 );
+         testQueue.enQueue( 47 );
+         testQueue.enQueue( 51 );
+         testQueue.enQueue( 57 );
+         //Test Peak and Pop
+         System.out.println( "The front of the Queue: " + testQueue.peek() );      // 57
+         System.out.println( "Removing front thing: " + testQueue.deQueue() );         // 57 removed
+         System.out.println( "The front of the Queue: " + testQueue.peek() );      // 51
+         System.out.println( "Removing front thing: " + testQueue.deQueue() );         // 51 removed
+         System.out.println( "Current Stack: " + testQueue.toString());
+         System.out.println( "The front of the Queue: " + testQueue.peek() );      // 47
+         System.out.println( "Removing front thing: " + testQueue.deQueue() );         // 47 removed
+         System.out.println( "The front of the Queue: " + testQueue.peek() );      // 43
+         testQueue.enQueue( testQueue.deQueue() + testQueue.deQueue() );
+         System.out.println( "The front of the Queue: " + testQueue.peek() );      // what'll it be?
+         System.out.println( "Current Stack: " + testQueue.toString());
+      }
+
+   }
